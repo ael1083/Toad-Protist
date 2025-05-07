e388ec8f7c9ae869d6ac35a130f8277e03a9a2b1
ael1083 <ael1083@usnh.edu>
2025-05-07 16:27:49 -0400

    Update blast.sh

--- blast.sh
+++ blast.sh
@@ -1,4 +1,3 @@
-```bash
 #!/bin/blast
 
 #Make BLAST database out of contig assembly
@@ -12,4 +11,3 @@ less 16S_vs_contigs_6.tsv
 #Create output file for blobtools
 /usr/local/bin/blast-ncbi-nt.sh contigs.fasta
 less contigs.fasta.vs.nt.cul5.1e5.megablast.out
-```
